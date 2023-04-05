01c0907cc5030f5cad364ee2470a2767189c3058
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2023-04-05 23:26:15 +0200

    fixed environment problems

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const APP_CONFIG = {
-  production: true,
-  environment: 'PROD'
+    production: true,
+    environment: 'PROD',
+    mapsFolderPath: 'maps',
+    mapPath: 'map.png',
+    fogOfWarPath: 'fog_of_war.png',
+    mapWithFogOfWarPath: 'map_with_fog_of_war.png',
+    dmNotesPath: 'dm_notes.png',
+    playerNotesPath: 'player_notes.png',
+    settingsPath: 'settings.json'
 };
--- src/environments/environment.web.prod.ts
+++ src/environments/environment.web.prod.ts
@@ -1,4 +1,11 @@
 export const APP_CONFIG = {
-  production: true,
-  environment: 'WEB-PROD'
+    production: true,
+    environment: 'WEB-PROD',
+    mapsFolderPath: 'maps',
+    mapPath: 'map.png',
+    fogOfWarPath: 'fog_of_war.png',
+    mapWithFogOfWarPath: 'map_with_fog_of_war.png',
+    dmNotesPath: 'dm_notes.png',
+    playerNotesPath: 'player_notes.png',
+    settingsPath: 'settings.json'
 };
